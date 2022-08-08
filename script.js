aec2c45948620efb9a7ff71fd00f499f41d84a2c
Bernie Petters <bp4924@gmail.com>
2022-08-07 22:28:49 -0400

    restore data works

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ let taskHour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 console.log(today);
 
 function checkTime() {
-  var nowHour = 12; // hard-code nowHour for t/s purposes
-  //  var nowHour = moment().format("HH"); // get 24-hour clock #
+  //  var nowHour = 10; // hard-code nowHour for t/s purposes
+  var nowHour = moment().format("HH"); // get 24-hour clock #
   console.log(nowHour);
 
   for (let i = 0; i < taskHour.length; i++) {
@@ -23,8 +23,9 @@ function checkTime() {
     // find taskTime
     var taskTime = $(stringId);
 
-    //add .past class
+    // add class based on current time
     if (num < nowHour) {
+      //add past class
       console.log("past");
       taskTime.addClass("past");
       // set textarea attribute to disabled -- input?
@@ -33,7 +34,7 @@ function checkTime() {
       console.log("future");
       taskTime.addClass("future");
     } else {
-      //<input type="text" id="task9"> add present class
+      // add present class
       console.log("now");
       taskTime.addClass("now");
     }
@@ -52,10 +53,17 @@ $("button").click(function () {
 });
 
 function restoreItems() {
-  var item = localStorage.getItem("#task12");
-  console.log(item);
-  $("#task12").text = item.value;
+  for (let i = 0; i < taskHour.length; i++) {
+    num = i + 9;
+    var stringI = num.toString();
+    stringId = `task${stringI}`;
+    console.log(stringI);
+    var item = localStorage.getItem(stringId);
+    console.log(item);
+    var restoreText = document.getElementById(stringId);
+    restoreText.value = item;
+  }
 }
 
 checkTime();
-//restoreItems();
+restoreItems();
