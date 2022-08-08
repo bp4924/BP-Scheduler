813a16976a330ae09301035c5eb8309552b0c508
Bernie Petters <bp4924@gmail.com>
2022-08-07 20:14:11 -0400

    save to local storage

--- script.js
+++ script.js
@@ -1,43 +1,61 @@
 const currentDayEl = $("#currentDay");
-let taskTime9 = $("#9");
 
+let stringId;
 let today = moment().format("dddd, MMMM Do YYYY");
 currentDayEl.text(today);
 
-let taskId = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+let taskHour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 console.log(today);
 
 function checkTime() {
-  var nowHour = 10;
-  //  var nowHour = moment().format("hh");
+  var nowHour = 12; // hard-code nowHour for t/s purposes
+  //  var nowHour = moment().format("HH"); // get 24-hour clock #
   console.log(nowHour);
 
-  for (var i = 0; i < taskId.length; i++) {
-    console.log(taskId[i]);
+  for (let i = 0; i < taskHour.length; i++) {
+    //generate stringID
     num = i + 9;
     var stringI = num.toString();
-    var stringId = `#${stringI}`;
-
+    stringId = `#${stringI}`;
     console.log(stringId);
-    var taskTime = $(stringId);
-    console.log(taskTime); //returns null object. How can I return desired table row element?
 
+    // find taskTime
+    var taskTime = $(stringId);
+
+    //add .past class
     if (num < nowHour) {
       console.log("past");
       taskTime.addClass("past");
-
-      //add .past class
+      // set textarea attribute to disabled -- input?
     } else if (num > nowHour) {
+      // add future class
       console.log("future");
       taskTime.addClass("future");
-      // add future class
     } else {
+      //<input type="text" id="task9"> add present class
       console.log("now");
       taskTime.addClass("now");
-      // add present class
     }
   }
 }
 
+$("button").click(function () {
+  var trId = $(this).closest("tr").attr("id"); // table row ID
+  // save content from current row task to local storage*/
+  stringId = `task${trId}`;
+  console.log("stringID: " + stringId);
+  var savText = document.getElementById(stringId).value;
+  console.log(savText);
+
+  localStorage.setItem(stringId, savText);
+});
+
+function restoreItems() {
+  var item = localStorage.getItem("#task12");
+  console.log(item);
+  $("#task12").text = item.value;
+}
+
 checkTime();
+//restoreItems();
